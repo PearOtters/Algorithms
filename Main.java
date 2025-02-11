b587033172c34ff03346ea45904809da3131333b
PearOtters <pierrealexis.outters@gmail.com>
2025-02-11 16:13:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Map<String, int[]> textfiles = new LinkedHashMap<>();
+        textfiles.put("int10.txt", SortingAlgorithms.getInputFromText("int10.txt")); 
+        textfiles.put("int50.txt", SortingAlgorithms.getInputFromText("int50.txt")); 
+        textfiles.put("int100.txt", SortingAlgorithms.getInputFromText("int100.txt")); 
+        textfiles.put("int1000.txt", SortingAlgorithms.getInputFromText("int1000.txt")); 
+        textfiles.put("bad.txt", SortingAlgorithms.getInputFromText("bad.txt")); 
+        textfiles.put("int20k.txt", SortingAlgorithms.getInputFromText("int20k.txt")); 
+        textfiles.put("dutch.txt", SortingAlgorithms.getInputFromText("dutch.txt")); 
+        textfiles.put("int500k.txt", SortingAlgorithms.getInputFromText("int500k.txt")); 
+        textfiles.put("intBig.txt", SortingAlgorithms.getInputFromText("intBig.txt")); 
+
+        //boolean isSorted = SortingAlgorithms.testSortingAlgorithm(SortingAlgorithms::hybridMergeSort, textfiles.get("int100.txt").clone(), true);
+        //System.out.println("Is sorted: " + isSorted);
+
+        testAlgorithms(textfiles.get("int100.txt"));
+        testTimes(textfiles);
+    }
+
+    public static void testAlgorithms(int[] input)
+    {
+        for (String funcName : SortingAlgorithms.funcs.keySet())
+        {
+            System.out.println(funcName);
+            boolean isSorted = SortingAlgorithms.testSortingAlgorithm(SortingAlgorithms.funcs.get(funcName), input.clone(), false);
+            System.out.println("Is sorted: " + isSorted);
+            System.out.println("____________________________________________________________________\n");
+        }
+    }
+
+    public static void testTimes(Map<String, int[]> textfiles)
+    {
+        for (String textfile : textfiles.keySet())
+        {
+            System.out.println("Testing for " + textfile + "\n");
+            for (String funcName : SortingAlgorithms.funcs.keySet())
+            {
+                long algDuration = SortingAlgorithms.timeAlgorithm(SortingAlgorithms.funcs.get(funcName), textfiles.get(textfile).clone(), false);
+                System.out.println(funcName + " took " + algDuration + "ms to sort");
+            }
+            System.out.println("\n____________________________________________________________________\n\n");
+        }
+    }
+}
---
+++ SortingAlgorithms.java
@@ -0,0 +1,300 @@
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SortingAlgorithms
+{
+    public static final Map<String, Function<int[], int[]>> funcs = new LinkedHashMap<>();
+    static {
+        funcs.put("Insertion sort", SortingAlgorithms::insertionSort); 
+        funcs.put("Selection sort", SortingAlgorithms::selectionSort); 
+        funcs.put("Shell sort", SortingAlgorithms::shellSort); 
+        funcs.put("Merge sort", SortingAlgorithms::mergeSort); 
+        funcs.put("Merge-Insertion sort", SortingAlgorithms::hybridMergeSort); 
+        funcs.put("Bottom Up Merge sort", SortingAlgorithms::bottomUpMergeSort); 
+        funcs.put("Quick sort", SortingAlgorithms::quickSort); 
+        funcs.put("Median of 3 Quick sort", SortingAlgorithms::medianOf3); 
+        funcs.put("Dutch Flag Quick sort", SortingAlgorithms::dutchFlag); 
+        funcs.put("Quick-Insertion sort", SortingAlgorithms::hybridQuickSort); 
+    }
+
+    public static int[] getInputFromText(String filename)
+    {
+        List<Integer> list = new ArrayList<Integer>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(Integer.parseInt(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int[] input = new int[list.size()];
+        for (int i = 0; i < input.length; i++)
+        {
+            input[i] = list.get(i);
+        }
+        return input;
+    }
+
+    private static boolean isSorted(int[] input, int[] result) {
+        Arrays.sort(input);
+        try
+        {
+            for (int i = 0; i < input.length - 1; i++) {
+                if (input[i] != result[i]) {
+                    return false;
+                }
+            }
+        } catch(Exception e) { return false; }
+        return true;
+    }
+
+    public static boolean testSortingAlgorithm(Function<int[], int[]> func, int[] input, boolean print)
+    {
+        int[] result = func.apply(Arrays.copyOf(input, input.length));
+        if (print)
+        {
+            System.out.println("Input:  " + Arrays.toString(input));
+            System.out.println("Result: " + Arrays.toString(result));
+        }
+        return isSorted(input, result);
+    }
+
+    public static long timeAlgorithm(Function<int[], int[]> func, int[] input, boolean print)
+    {
+        long startTime = System.currentTimeMillis();
+        if (print) { System.out.println("Start Time: " + startTime); }
+        func.apply(Arrays.copyOf(input, input.length));
+        long endTime = System.currentTimeMillis();
+        if (print) { System.out.println("End Time: " + endTime); }
+        return endTime - startTime;
+    }
+
+    public static int[] insertionSort(int[] input)
+    {
+        int n = input.length;
+        for (int j = 1; j < n; j++)
+        {
+            int key = input[j];
+            int i = j - 1;
+            while (i >= 0 && input[i] > key)
+            {
+                input[i + 1] = input[i];
+                i--;
+            }
+            input[i + 1] = key;
+        }
+        return input;
+    }
+
+    public static int[] selectionSort(int[] input)
+    {
+        int n = input.length;
+        for (int i = 0; i < n; i++)
+        {
+            int minIndex = i;
+            for (int j = i; j < n; j++)
+            {
+                if (input[j] < input[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            int temp = input[i];
+            input[i] = input[minIndex];
+            input[minIndex] = temp;
+        }
+        return input;
+    }
+    
+    public static int[] shellSort(int[] input)
+    {
+        int h = 1; int n = input.length;
+        while (h < n / 3)
+        {
+            h = 3 * h + 1;
+        }
+        while (h >= 1)
+        {
+            for (int i = h; i < n; i++)
+            {
+                for (int j = i; j >= h && input[j] < input[j-h]; j-=h)
+                {
+                    int temp = input[j];
+                    input[j] = input[j - h];
+                    input[j - h] = temp;
+                }
+            }
+            h /= 3;
+        }
+        return input;
+    }
+
+    public static int[] mergeSort(int[] input)
+    {
+        mergeSort(input, 0, input.length - 1);
+        return input;
+    }
+
+    private static void mergeSort(int[] input, int p, int r)
+    {
+        if (p < r)
+        {
+            int q = (p + r) / 2;
+            mergeSort(input, p, q);
+            mergeSort(input, q + 1, r);
+            merge(input, p, q, r);
+        }
+    }
+
+    private static void merge(int[] input, int p, int q, int r)
+    {
+        int[] L = IntStream.concat(Arrays.stream(Arrays.copyOfRange(input, p, q + 1)), Arrays.stream(new int[]{ Integer.MAX_VALUE })).toArray();
+        int[] R = IntStream.concat(Arrays.stream(Arrays.copyOfRange(input, q + 1, r + 1)), Arrays.stream(new int[]{ Integer.MAX_VALUE })).toArray();
+        int i = 0, j = 0;
+        for (int k = p; k <= r; k++)
+        {
+            if (L[i] <= R[j])
+            {
+                input[k] = L[i];
+                i++;
+            }
+            else
+            {
+                input[k] = R[j];
+                j++;
+            }
+        }
+    }
+
+    public static int[] hybridMergeSort(int[] input)
+    {
+        hybridMergeSort(input, 0, input.length - 1);
+        return input;
+    }
+
+    private static void hybridMergeSort(int[] input, int p, int r)
+    {
+        if (p < r)
+        {
+            int q = (p + r) / 2;
+            hybridMergeSort(input, p, q);
+            hybridMergeSort(input, q + 1, r);
+            Hybridmerge(input, p, q, r);
+        }
+    }
+
+    private static void Hybridmerge(int[] input, int p, int q, int r)
+    {
+        if (r - p + 1 > 32)
+        {
+            int[] L = IntStream.concat(Arrays.stream(Arrays.copyOfRange(input, p, q + 1)), Arrays.stream(new int[]{ Integer.MAX_VALUE })).toArray();
+            int[] R = IntStream.concat(Arrays.stream(Arrays.copyOfRange(input, q + 1, r + 1)), Arrays.stream(new int[]{ Integer.MAX_VALUE })).toArray();
+            int i = 0, j = 0;
+            for (int k = p; k <= r; k++)
+            {
+                if (L[i] <= R[j])
+                {
+                    input[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    input[k] = R[j];
+                    j++;
+                }
+            }
+        }
+        else
+        {
+            for (int j = p + 1; j <= r; j++)
+            {
+                int key = input[j];
+                int i = j - 1;
+                while (i >= p && input[i] > key)
+                {
+                    input[i + 1] = input[i];
+                    i--;
+                }
+                input[i + 1] = key;
+            }
+        }
+    }
+
+    public static int[] bottomUpMergeSort(int[] input)
+    {
+        bottomUpMergeSort(input, 0, input.length - 1);
+        return input;
+    }
+
+    public static void bottomUpMergeSort(int[] input, int p, int r)
+    {
+        int n = r - p + 1;
+        for (int sz = 1; sz < n; sz*=2)
+        {
+            for (p = 0; p < n-sz; p+=sz*2)
+            {
+                merge(input, p, p+sz-1, Math.min(p+sz*2-1, n-1));
+            }
+        }
+    }
+
+    public static int[] quickSort(int[] input)
+    {
+        quickSort(input, 0, input.length-1);
+        return input;
+    }
+
+    public static void quickSort(int[] input, int p, int r)
+    {
+        if (p < r)
+        {
+            int q = partition(input, p, r);
+            quickSort(input, p, q-1);
+            quickSort(input, q+1, r);
+        }
+    }
+
+    public static int partition(int[] input, int p, int r)
+    {
+        int x = input[r];
+        int i = p - 1;
+        for (int j = p; j < r; j++)
+        {
+            if (input[j] <= x)
+            {
+                i++;
+                int temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+            }
+        }
+        int temp = input[i + 1];
+        input[i + 1] = input[r];
+        input[r] = temp;
+        return i + 1;
+    }
+
+    public static int[] medianOf3(int[] input)
+    {
+        return input;
+    }
+
+    public static int[] dutchFlag(int[] input)
+    {
+        return input;
+    }
+
+    public static int[] hybridQuickSort(int[] input)
+    {
+        return input;
+    }
+}
